74396c0287e899179748a3c575d48b420b1e7ef9
Nikita <152098327+Nihihitik@users.noreply.github.com>
2025-04-23 14:52:12 +0300

    Обновлены POST и GET запросы

--- crud.py
+++ crud.py
@@ -14,10 +14,14 @@ async def get_message(message_id: int) -> str:
 
 @app.post("/message")
 async def create_message(message:  str) -> str:
+    current_index = len(messages_db)
+    messages_db[current_index] = message
+    return f"Сообщение добавлено!"
 
 @app.put("/message/{message_id}")
 async def update_message(message_id: str, message: str) -> str:
-    pass
+    messages_db[message_id] = message
+    return f"Сообщение изменено!"
 
 @app.delete("/message/{message_id}")
 async def delete_message(message_id: str) -> str:
