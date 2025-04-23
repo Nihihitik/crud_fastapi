dee4015a6eefab378f77975087f36693c29b7a88
Nikita <152098327+Nihihitik@users.noreply.github.com>
2025-04-23 16:38:57 +0300

    Добавлено HTTPException для обработки ошибок

--- crud.py
+++ crud.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -23,12 +24,15 @@ class Message(BaseModel):
     text: str
 
 @app.get('/')
-async def get_all_messages() -> dict:
-    return {'Сообщения': messages_db}
+async def get_all_messages() -> List[Message]:
+    return messages_db
 
 @app.get("/messages/{message_id}")
-async def get_message(message_id: int):
-    return messages_db[message_id]
+async def get_message(message_id: int) -> Message:
+    try:
+        return messages_db[message_id]
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Сообщение не найдено")
 
 @app.post("/message")
 async def create_message(message:  Message) -> str:
@@ -41,14 +45,20 @@ async def create_message(message:  Message) -> str:
 
 @app.put("/message/{message_id}")
 async def update_message(message_id: int, message: str = Body()) -> str:
-    edit_message = messages_db[message_id]
-    edit_message.text = message
-    return f"Сообщение обновлено!"
+    try:
+        edit_message = messages_db[message_id]
+        edit_message.text = message
+        return f"Message updated!"
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Message not found")
 
 @app.delete("/message/{message_id}")
 async def delete_message(message_id: int) -> str:
-    messages_db.pop(message_id)
-    return f"Сообщение с ID: {message_id} было удалено!"
+    try:
+        messages_db.pop(message_id)
+        return f"Message ID={message_id} deleted!"
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Message not found")
 
 @app.delete("/")
 async def kill_message_all() -> str:
