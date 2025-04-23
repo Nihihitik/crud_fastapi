5bcc41a46d4f821cfdd4149ea10935569a101fec
Nikita <152098327+Nihihitik@users.noreply.github.com>
2025-04-23 14:59:01 +0300

    Обновлены DELETE-методы

--- crud.py
+++ crud.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -19,14 +19,16 @@ async def create_message(message:  str) -> str:
     return f"Сообщение добавлено!"
 
 @app.put("/message/{message_id}")
-async def update_message(message_id: str, message: str) -> str:
+async def update_message(message_id: str, message: str = Body()) -> str:
     messages_db[message_id] = message
     return f"Сообщение изменено!"
 
 @app.delete("/message/{message_id}")
 async def delete_message(message_id: str) -> str:
-    pass
+    messages_db.pop(message_id)
+    return f"Сообщение с ID: {message_id} было удалено!"
 
 @app.delete("/")
 async def kill_message_all() -> str:
-    pass
+    messages_db.clear()
+    return "Все сообщения удалены"
